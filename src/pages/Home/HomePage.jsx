a286f9be9ff7e31942b41fc2713e10d9167d1144
muzaffarovhayotbek <hayotbek0511@gmail.com>
2025-03-08 17:44:08 +0500

    home page

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import './HomePage.css'
+import React from 'react';
+import './HomePage.css';
+import dummyjson from '../../assets/db.json';
+
 function HomePage() {
   return (
-    <div className='homePage'>
-      <div className="container">
+    <div className="homePage">
+      <div className="container homePage__container">
         <h2>Arzon narxlar</h2>
+        {dummyjson.products.map((product) => (
+          <li key={product.id} className="product">
+            <img
+              src={product.thumbnail}
+              alt={product.title}
+              className="product-image"
+            />
+            <div className="product-info">
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <p>Narxi: ${product.price}</p>
+            </div>
+          </li>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
